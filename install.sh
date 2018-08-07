b7fdc55efce470898a2dc77c3488480780d3e309
microwavecausesaids <33128510+microwavecausesaids@users.noreply.github.com>
2018-08-07 22:20:15 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #nextcloud
-sudo add-apt-repository ppa:nextcloud-devs/client
-sudo apt-get update
-sudo apt-get install nextcloud
+sudo add-apt-repository ppa:nextcloud-devs/client -y && apt-get update && apt-get install -y nextcloud-client
+
+#keepassx
+sudo add-apt-repository ppa:eugenesan/ppa -y && sudo apt-get update && apt-get install -y keepassx
