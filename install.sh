14654b99bafabf7a371cd3a13ca6788aa0dd14ce
microwavecausesaids <33128510+microwavecausesaids@users.noreply.github.com>
2018-08-23 22:06:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #nextcloud
-sudo add-apt-repository ppa:nextcloud-devs/client -y && apt-get update && apt-get install -y nextcloud-client
-
+sudo add-apt-repository ppa:nextcloud-devs/client -y 
 #keepassx
-sudo add-apt-repository ppa:eugenesan/ppa -y && sudo apt-get update && apt-get install -y keepassx
+sudo add-apt-repository ppa:eugenesan/ppa -y
+
+
+apt-get update
+apt-get upgrade
 
 #firefox vlc  gnome-disk-utility zsh nethogs
-sudo apt-get install -y firefox vlc  gnome-disk-utility zsh nethogs kazam
+sudo apt-get install -y firefox vlc  gnome-disk-utility zsh nethogs kazam nextcloud-client keepassx
