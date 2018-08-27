fa9f2871cb0bacadd68aac434f6cb8ab70632196
microwavecausesaids <33128510+microwavecausesaids@users.noreply.github.com>
2018-08-27 13:36:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,7 @@ apt-get update
 apt-get upgrade
 
 #firefox vlc  gnome-disk-utility zsh nethogs
-sudo apt-get install -y firefox vlc  gnome-disk-utility zsh nethogs kazam nextcloud-client keepassx
+sudo apt-get install -y firefox vlc  gnome-disk-utility zsh nethogs kazam nextcloud-client keepassx arandr
+
+#monitor configuration
+xrandr --output DisplayPort-0 --off --output DVI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --off --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal
